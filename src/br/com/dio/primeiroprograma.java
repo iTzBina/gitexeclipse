760884053a1682dad4367c682b44ab10aac40897
iTzBina <csjunior100@hotmail.com>
2021-10-27 13:53:24 -0300

    teste

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -13,7 +13,7 @@ public class primeiroprograma {
 			
 			System.out.println(gato);
 			System.out.println(livros);
-			System.out.println("Github liberado com sucesso");			
+			System.out.println("Github liberado com sucesso!");			
 	}
 
 }
